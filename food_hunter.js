210ec5279eb2389f0376a3b4acbe036c2d5c0c8e
neaboo822 <ss1234534@hotmail.com>
2014-12-09 13:45:24 -0600

    Create food_hunter.js

---
+++ food_hunter.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#add').click(function () {
+        $('#search_row').append("<br><input class='form-control' id='Radius' placeholder='Enter your ingredient'>");
+    });
+});
